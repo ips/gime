b4f0b74b536f10b0eee97f126fa2b067514775e8
Marcin Karpezo <sirmacik@gmail.com>
2009-06-23 10:32:21 +0200

    gime-lzma little rewrite

--- python/gime-lzma.py
+++ python/gime-lzma.py
@@ -19,7 +19,7 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import sys
 import os
-from subprocess import Popen
+import subprocess
 
 if (len(sys.argv) > 1):
     name = sys.argv[1]
@@ -30,21 +30,21 @@ command = "lzma -S .gime"
 
 def compress(s):
     try:
-        print "Compressing %s ...\n" %name
-        compress = Popen(command+' '+s, shell=True)
-        os.waitpid(compress.pid, 0)
-        print "Done\n"
+        print "Compressing %s ..." %name
+        ccmd = "%s %s" % (command, s)
+        compress = subprocess.Popen(ccmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()[0]
+        print "Done"
     except IndexError: 
-            sys.stderr.write('Error: Wrong filename\n')
+            sys.stderr.write('Error: Wrong filename')
 
 def uncompress(s):
     try:
-        print "Decompressing %s ...\n" % name
-        uncompress = Popen(command+' -d '+s, shell=True)
-        os.waitpid(uncompress.pid, 0)
-        print "Done\n"
+        print "Decompressing %s ..." % name
+        uncmd = "%s -d %s" % (command, s)
+        uncompress = subprocess.Popen(uncmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()[0]
+        print "Done"
     except IndexError: 
-            sys.stderr.write('Error: Wrong filename\n')
+            sys.stderr.write('Error: Wrong filename')
 if name:
     if name.endswith('.gime'):
         uncompress(name)
